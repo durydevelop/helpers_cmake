3fe059dd498b139db3923a1dd2542a8784a764c0
fabio <develop@dury.it>
2024-02-28 19:43:35 +0100

    dpplib

--- DppLibFinder.cmake
+++ DppLibFinder.cmake
@@ -1,14 +1,14 @@
 ######
-# dpplib
+# DPPLIB
 ##[[ Sorgenti in locale
-## usa dpplib_ROOT
-message_c(${BOLD_MAGENTA} "Finding dpplib...")
-if (NOT ${dpplib_ROOT} STREQUAL "")
-    message_c(${BOLD_MAGENTA} "dpplib_ROOT set to ${dpplib_ROOT}")
+## usa DPPLIB_ROOT
+message_c(${BOLD_MAGENTA} "Finding DPPLIB...")
+if (NOT ${DPPLIB_ROOT} STREQUAL "")
+    message_c(${BOLD_MAGENTA} "DPPLIB_ROOT set to ${DPPLIB_ROOT}")
 endif()
 
-if (NOT EXISTS ${dpplib_ROOT})
-    message(FATAL_ERROR "dpplib_ROOT not found. Please set correct dpplib_ROOT path")
+if (NOT EXISTS ${DPPLIB_ROOT})
+    message(FATAL_ERROR "DPPLIB_ROOT not found. Please set correct DPPLIB_ROOT path")
 endif()
 ##]]
 
@@ -18,27 +18,27 @@ set(FETCHCONTENT_QUIET OFF)
 set(FETCHCONTENT_UPDATES_DISCONNECTED OFF)
 set(FETCHCONTENT_BASE_DIR deps)
 FetchContent_Declare(
-    dpplib
-    GIT_REPOSITORY https://dury:CFg3BdZs3Wt8zeVobxCc@gitlab.com/durydevelop/cpp/lib/dpplib.git
+    DPPLIB
+    GIT_REPOSITORY https://dury:CFg3BdZs3Wt8zeVobxCc@gitlab.com/durydevelop/cpp/lib/DPPLIB.git
     #GIT_PROGRESS TRUE
 )
 # Check if population has already been performed
-FetchContent_GetProperties(dpplib)
-if(NOT dpplib_POPULATED)
+FetchContent_GetProperties(DPPLIB)
+if(NOT DPPLIB_POPULATED)
     message(${DSTATUS} "Not populated")
-    FetchContent_Populate(dpplib)
+    FetchContent_Populate(DPPLIB)
 endif()
-FetchContent_MakeAvailable(dpplib)
+FetchContent_MakeAvailable(DPPLIB)
 
-add_subdirectory(${dpplib_ROOT} deps/dpplib)
-include_directories(${dpplib_ROOT}/include)
-target_link_libraries(${PROJECT_NAME} PRIVATE dpplib)
+add_subdirectory(${DPPLIB_ROOT} deps/DPPLIB)
+include_directories(${DPPLIB_ROOT}/include)
+target_link_libraries(${PROJECT_NAME} PRIVATE DPPLIB)
 #]]
 
-add_subdirectory(${dpplib_ROOT} deps/dpplib)
-include_directories(${PROJECT_NAME} PUBLIC ${dpplib_ROOT}/include)
-get_target_property(dpplib_SOURCES dpplib SOURCES)
-target_sources(${PROJECT_NAME} PUBLIC ${dpplib_SOURCES})
+add_subdirectory(${DPPLIB_ROOT} deps/DPPLIB)
+include_directories(${PROJECT_NAME} PUBLIC ${DPPLIB_ROOT}/include)
+get_target_property(DPPLIB_SOURCES ${PROJECT_NAME} SOURCES)
+target_sources(${PROJECT_NAME} PUBLIC ${DPPLIB_SOURCES})
 
 include_directories(${PROJECT_NAME} PUBLIC ${Boost_INCLUDE_DIR})
 
